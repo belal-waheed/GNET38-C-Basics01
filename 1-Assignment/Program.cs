9d9e8ea79832e2537c1795fa60b034f59037dd02
belal-waheed <belwaheed@gmail.com>
2026-08-26 11:07:16 +0300

    Question 01

--- 1-Assignment/Program.cs
+++ 1-Assignment/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace _1_Assignment
+namespace _1_Assignment
 {
+    class Book
+    {
+        public string? Title;
+        public int Pages;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 01
+            // Create a Book class with a Title (string) and Pages (int). Create a Book object and store it
+            // in a variable of type object. Print it.
+             
+            //object book = new Book() { Title = "Clean Code", Pages = 464 };
+            //Console.WriteLine(book);
+            #endregion
+
         }
     }
 }
